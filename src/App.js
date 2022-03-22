db35d8ea720d3eee6cd974fdc9c296cb345f30f2
Fernando <ferg.developer@gmail.com>
2022-03-22 11:35:53 -0400

    React Practice 1 Done

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  // 
+  increment = () => {
+    this.setState({ count: this.state.count +1 })
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.increment}>
+          Increment
+        </button>
+        {this.state.count}
+      </div>
+    );
+  }
+}
+
+export default App;
